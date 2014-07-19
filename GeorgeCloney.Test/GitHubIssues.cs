d117e295500c20de72fe38e7ec051c45b7e86867
Jason Duffett <github@jasonduffett.org>
2014-07-19 17:13:52 +0100

    Test base class members are cloned
    Unit test for issue #2

--- GeorgeCloney.Test/GitHubIssues.cs
+++ GeorgeCloney.Test/GitHubIssues.cs
@@ -15,6 +15,21 @@ namespace GeorgeCloney.Test
             Assert.IsInstanceOfType(clone.SomeObject, typeof(Bar));
         }
 
+        [TestMethod]
+        [Description("GitHub Issue #2")]
+        public void ClonePropertiesFromBaseClasses()
+        {
+            var q = new BarRoomQueen
+                {
+                    SomeValue = 15,
+                    FromMemphis = true
+                };
+
+            var clone = q.DeepCloneWithoutSerialization();
+
+            Assert.AreEqual(q.FromMemphis, clone.FromMemphis);
+            Assert.AreEqual(q.SomeValue, clone.SomeValue);
+        }
 
         class Foo
         {
@@ -24,6 +39,11 @@ namespace GeorgeCloney.Test
         class Bar
         {
             public int SomeValue { get; set; }
+        }
+
+        class BarRoomQueen : Bar
+        {
+            public bool FromMemphis { get; set; }
         }
     }
 }
